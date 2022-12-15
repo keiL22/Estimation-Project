7ed191fb552ab2242da404501bf72f75d5dbe1b8
Keisuke <91330292+KeisukeL@users.noreply.github.com>
2022-12-14 21:07:05 -0600

    Delete sourceseBox.js

--- Javascript/sourceseBox.js
+++
@@ -1,9 +0,0 @@
-checked();
-
-function checked() {
-	
-	var checkbox = document.getElementById("sourcesBox");
-	var label = document.getElementById("sourcesLabel");
-	
-	label.innerHTML = checkbox.checked ? "&#x25B2; Hide Sources" : "&#x25BC; Display Sources";
-}
